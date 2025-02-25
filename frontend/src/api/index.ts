e59a0d989d3d89ea9886585855d98c2e7bf81233
Maxim Polyak <mpolyak7@gmail.com>
2025-02-26 01:51:52 +0200

    m5

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_API_URL
+const apiUrl = import.meta.env.VITE_API_URL || "https://api.polyak.pp.ua"
 // TODO: сделать проверку на существование VITE_APP_API_URL если что вывести заметную ошибку
 export const api = axios.create({
   baseURL: apiUrl,
